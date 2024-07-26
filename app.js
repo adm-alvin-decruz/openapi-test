cd5f140b4e2d4cc890554072b31a1911b5b9098f
Kwan Oun Liong <kwanoun.liong@1ITDMACDQ2K1H2YV1.local>
2024-07-26 11:44:04 +0800

    [CDT-24] feat: cleanup gitignore

--- app.js
+++ app.js
@@ -12,15 +12,6 @@ app.use('/', membershipRoutes)
 
 const handler = serverless(app);
 
-const port = 3010;
-const startServer = async () => {
-    app.listen(port, () => {
-        console.log(`Server running on http://localhost:${port}`);
-      });
-}
-
-startServer();
-
 module.exports.handler = (event, context, callback) => {
     const response = handler(event, context, callback);
     return response;
