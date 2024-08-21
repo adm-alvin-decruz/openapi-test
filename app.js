1fdec609547890dcdfec1d828a4310ea57ba7610
Kwan Oun Liong <kwanoun.liong@1itdllepf3cjlrt.mandai.com>
2024-08-21 09:27:49 +0800

    [CDT-51] feat: update route use v1/ciam path

--- app.js
+++ app.js
@@ -9,10 +9,19 @@ app.use(cors())
 app.use(express.json());
 
 // use routes
-app.use('/', membershipRoutes)
-app.use('/', userRoutes)
+app.use('/v1/ciam/', membershipRoutes)
+app.use('/v1/ciam/', userRoutes)
 
 const handler = serverless(app);
+
+const port = 3010;
+const startServer = async () => {
+    app.listen(port, () => {
+        console.log(`Server running on http://localhost:${port}`);
+      });
+}
+
+startServer();
 
 module.exports.handler = (event, context, callback) => {
     const response = handler(event, context, callback);
