c3ead0c033985a67c67ef995319fa789b8ab7944
Kwan Oun Liong <kwanoun.liong@1ITDMACDQ2K1H2YV1.local>
2024-09-25 09:47:22 +0800

    [CDT-34] feat: update cardface firstname follow by lastname

--- src/api/users/usersServices.js
+++ src/api/users/usersServices.js
@@ -370,7 +370,7 @@ async function prepareWPCardfaceInvoke(req){
    let dob = commonService.convertDateHyphenFormat(req.body.dob);
    // event data
    const event = {
-      name: req.body.lastName +' '+ req.body.firstName,
+      name: req.body.firstName +' '+ req.body.lastName,
       dateOfBirth: dob,
       mandaiId: req.body.mandaiID
    };
