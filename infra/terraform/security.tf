1e1ada2b2ab53db361d1b5818c8f7bf26301a508
amar kant monu <amar.kantmonu@mandai.com>
2024-10-21 10:41:09 +0800

    CDT-81-ciam-sqs

--- infra/terraform/security.tf
+++ infra/terraform/security.tf
@@ -22,3 +22,11 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
+resource "aws_vpc_security_group_egress_rule" "allow_all_sql_traffic_ipv4" {
+  security_group_id = aws_security_group.lambda.id
+  cidr_ipv4         = data.terraform_remote_state.network.outputs.vpc_cidr_block
+  ip_protocol       = "tcp"
+  from_port         = data.terraform_remote_state.rds.outputs.cluster_port
+  to_port           = data.terraform_remote_state.rds.outputs.cluster_port
+}
+
