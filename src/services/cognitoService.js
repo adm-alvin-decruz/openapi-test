a7868421f1759616ccc1400ce3965b6f96b3201a
vdr-synodus-quangnhd <quangnhd@synodus.com>
2024-12-19 23:56:13 +0700

    CIAM-87: update return signup success message

--- src/services/cognitoService.js
+++ src/services/cognitoService.js
@@ -110,7 +110,7 @@ class Cognito {
       Permanent: true
     });
     try {
-      await client.send(setPasswordParams);
+      return await client.send(setPasswordParams);
     } catch (error) {
       loggerService.error(
           `cognitoService.cognitoAdminSetUserPassword Error: ${error}`
@@ -128,7 +128,7 @@ class Cognito {
       Username: email,
     });
     try {
-      await client.send(setDeleteUserParams);
+      return await client.send(setDeleteUserParams);
     } catch (error) {
       loggerService.error(
           `cognitoService.cognitoAdminDeleteUser Error: ${error}`
