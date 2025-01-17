e68d33c4f168a4fb126b1812c23c8f6adf3031e8
vdr-synodus-quangnhd <quangnhd@synodus.com>
2025-01-17 10:18:43 +0700

    [CIAM-92] feat: update regex password & callbackUrl login

--- src/utils/common.js
+++ src/utils/common.js
@@ -37,7 +37,7 @@ const getGroup = (group) => {
 
 const passwordPattern = (password) => {
   const regexPasswordValid = new RegExp(
-    '^(?=.*\\d)(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[A-Z])(?=.*[a-z]).{8,}$',
+    '^(?=.*[a-z])(?=.*[A-Z]).{8,}$',
     "g"
   );
   return regexPasswordValid.test(password.toString());
