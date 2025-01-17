334d186d1896709ec496ce29c64005d96f69eed5
Darren Leong <darren.leong@mandai.com>
2025-01-17 09:06:49 +0800

    [CIAM-163] feat: Create service for create and update membership pass endpoints

---
+++ src/api/users/userMembershipPassJob.js
@@ -0,0 +1,55 @@
+const UserMembershipPassService = require("./userMembershipPassService");
+// const { messageLang } = require("../../utils/common");
+
+class UserCreateMembershipPassJob {
+  success(lang = "en") {
+    return {
+      membership: {
+        code: 200,
+        mwgCode: "MWG_CIAM_CREATE_MEMBERSHIP_PASS_SUCCESS",
+        // message: messageLang("request_reset_password_success", lang),
+        message: "Successfully created membership pass",
+      },
+      status: "success",
+      statusCode: 200,
+    };
+  }
+
+  async perform(req) {
+    try {
+      await UserMembershipPassService.create(req);
+      return this.success(req.body.language);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+class UserUpdateMembershipPassJob {
+  success(lang = "en") {
+    return {
+      membership: {
+        code: 200,
+        mwgCode: "MWG_CIAM_UPDATE_MEMBERSHIP_PASS_SUCCESS",
+        message: "Successfully updated membership pass",
+      },
+      status: "success",
+      statusCode: 200,
+    };
+  }
+
+  async perform(req) {
+    try {
+      await UserMembershipPassService.update(req);
+      return this.success(req.body.language);
+    } catch (error) {
+      const errorMessage = JSON.parse(error.message);
+      throw new Error(JSON.stringify(errorMessage));
+    }
+  }
+}
+
+module.exports = {
+  userCreateMembershipPassJob: new UserCreateMembershipPassJob(),
+  userUpdateMembershipPassJob: new UserUpdateMembershipPassJob(),
+};
