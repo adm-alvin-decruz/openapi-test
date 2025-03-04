ee8b35feecb8feb7c73111a641ebdb361dd563a7
Kwan Oun Liong <kwanoun.liong@1ITDMACDQ2K1H2YV1.local>
2025-03-04 15:05:35 +0800

    [CIAM-154] feat: removed accidentally commit file

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-const membershipRoutes = require('./src/api/memberships/membershipRoutes');
-const userRoutes = require('./src/api/users/userRoutes');
-const galaxyRoutes = require('./src/api/components/galaxy/galaxyRoutes');
-const supportRoutes = require('./src/api/supports/supportRoutes')
+const express = require("express");
+const cors = require("cors");
+const membershipRoutes = require("./src/api/memberships/membershipRoutes");
+const userRoutes = require("./src/api/users/userRoutes");
+const galaxyRoutes = require("./src/api/components/galaxy/galaxyRoutes");
+const supportRoutes = require("./src/api/supports/supportRoutes");
 const app = express();
-const serverless = require('serverless-http');
+const serverless = require("serverless-http");
 
-app.use(cors())
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
 // use routes
-app.use('/v1/ciam/', membershipRoutes);
-app.use('/v1/ciam/', userRoutes);
+app.use("/v1/ciam/", membershipRoutes);
+app.use("/v1/ciam/", userRoutes);
 // testing galaxy
-app.use('/v1/ciam/galaxy', galaxyRoutes); // dev & uat env allowed only.
+app.use("/v1/ciam/galaxy", galaxyRoutes); // dev & uat env allowed only.
 // support route
-app.use('/v1/ciam', supportRoutes); // controlled by app ID
+app.use("/v1/ciam", supportRoutes); // controlled by app ID
 
 const handler = serverless(app);
 
-const port = 3010;
-const startServer = async () => {
-    app.listen(port, () => {
-        console.log(`Server running on http://localhost:${port}`);
-      });
-}
-
-startServer();
-
 module.exports.handler = (event, context, callback) => {
-    const response = handler(event, context, callback);
-    return response;
+  const response = handler(event, context, callback);
+  return response;
 };
