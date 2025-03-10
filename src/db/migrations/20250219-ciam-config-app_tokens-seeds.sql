72f304bbce79e6513ad77efeca44cf99ad642662
Kwan Oun Liong <kwanoun.liong@1ITDMACDQ2K1H2YV1.local>
2025-03-10 20:50:08 +0800

    [CIAM-257] feat: update migrations for app tokens

--- src/db/migrations/20250219-ciam-config-app_tokens-seeds.sql
+++ src/db/migrations/20250219-ciam-config-app_tokens-seeds.sql
@@ -6,4 +6,4 @@ VALUES
 
 INSERT INTO `app_tokens` (`client`, `credentials`, `token`, `configuration`, `expires_at`, `created_at`, `updated_at`)
 VALUES
-	('nopCommerce', '{\"client_id\": \"membership_secret_key\", \"grant_type\": \"client_credentials\", \"client_secret\": \"fa7xt2ak1g7i1cb1r7h118j114gf9\"}', NULL, '{\"storeId\": \"1\"}', NULL, NULL, NULL);
+  ('nopCommerce', '{\"client_id\": \"membership_secret_key\", \"grant_type\": \"client_credentials\", \"client_secret\": \"fa7xt2ak1g7i1cb1r7h118j114gf9\"}', NULL, '{\"storeId\": \"1\"}', NULL, now(), now());
