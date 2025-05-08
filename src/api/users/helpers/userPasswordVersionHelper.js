25232b020519da436c8e51d3703dd7ccdb735e9f
vdr-synodus-quangnhd <quangnhd@synodus.com>
2025-05-08 14:57:10 +0700

    CIAM-303 feat: organize source with helper function

---
+++ src/api/users/helpers/userPasswordVersionHelper.js
@@ -0,0 +1,70 @@
+const passwordService = require("../userPasswordService");
+const passwordVersionModel = require("../../../db/models/passwordVersionModel");
+
+async function isPasswordVersionExisted(
+  passwordInput,
+  listPasswordsByVersion,
+  versionCounting
+) {
+  if (!listPasswordsByVersion || !listPasswordsByVersion.length) {
+    return false;
+  }
+  if (versionCounting < 1) {
+    return false;
+  }
+
+  //2. check listPasswordsByVersion input is existed in list password or not - if existed return false
+  for (const pass of listPasswordsByVersion) {
+    const isPasswordExisted = await passwordService.comparePassword(
+      passwordInput,
+      pass.password_hash
+    );
+
+    if (isPasswordExisted) {
+      return true;
+    }
+  }
+  return false;
+}
+
+async function updatePasswordVersionAccordingly(
+  userId,
+  passwordHashed,
+  versionCounting,
+  passwordsByConfigVersion,
+  passwordsMaster
+) {
+  //  3.a create new record with version === config version number
+  await passwordVersionModel.create(userId, passwordHashed, versionCounting);
+
+  //  3.b modify list password master to mark version
+  //     3.b.i if idx existed in list password -> version = config version - (idx + 1)
+  //     3.b.ii if idx non existed in list password -> version = -1
+  const modifyVersionPasswordMaster = [];
+  const passwordsHash = passwordsByConfigVersion.map(
+    (pass) => pass.password_hash
+  );
+  for (let idx = 0; idx < passwordsMaster.length; idx++) {
+    if (passwordsHash.includes(passwordsMaster[idx].password_hash)) {
+      modifyVersionPasswordMaster.push({
+        ...passwordsMaster[idx],
+        version: versionCounting - 1 - idx,
+      });
+    } else {
+      modifyVersionPasswordMaster.push({
+        ...passwordsMaster[idx],
+        version: -1,
+      });
+    }
+  }
+  for (const pass of modifyVersionPasswordMaster) {
+    await passwordVersionModel.updateVersion(userId, pass.id, pass.version);
+  }
+
+  //4. delete password has negative
+  await passwordVersionModel.deleteVersionNegativeByUserID(userId);
+}
+module.exports = {
+  isPasswordVersionExisted,
+  updatePasswordVersionAccordingly,
+};
--- src/api/users/userPasswordVersionService.js
+++ src/api/users/userPasswordVersionService.js
@@ -2,65 +2,41 @@ require("dotenv").config();
 const passwordVersionModel = require("../../db/models/passwordVersionModel");
 const passwordService = require("./userPasswordService");
 const configsModel = require("../../db/models/configsModel");
+const { isPasswordVersionExisted, updatePasswordVersionAccordingly} = require("./helpers/userPasswordVersionHelper");
 
 class UserPasswordVersionService {
   async passwordValidProcessing(userId, password) {
     const passwordInfo = await passwordVersionModel.findByUserId(userId);
     const passHashed = await passwordService.hashPassword(password);
-
-    if (!passwordInfo || !passwordInfo.length) {
-      //insert password when data is empty
-      await passwordVersionModel.create(userId, passHashed, 1);
-      return false;
-    }
-    const config = await configsModel.findByConfigKey("password_version", "version");
+    const config = await configsModel.findByConfigKey(
+      "password_version",
+      "version"
+    );
     const versionCounting = config && config.value ? config.value : 0;
-    if (versionCounting < 1) {
-      return false;
-    }
-
-    //1. get list password to check by config number
+    //1. get list listPasswordsByVersion to check by config number
     const listPasswordsByConfigVersion = passwordInfo.slice(0, versionCounting);
 
-    //2. check password input is existed in list password or not - if existed return false
-    for (const pass of listPasswordsByConfigVersion) {
-      const isPasswordExisted = await passwordService.comparePassword(password, pass.password_hash);
+    //2. check password input is existed in list password by config - if existed return false
+    const passwordVersionExisted = await isPasswordVersionExisted(
+      password,
+      listPasswordsByConfigVersion,
+      versionCounting
+    );
 
-      if (isPasswordExisted) {
-        return true;
-      }
+    //stop process if password is existed
+    if (passwordVersionExisted) {
+      return true;
     }
 
-    //3. if no exists
-    //  3.a create new record with version === config version number
-    await passwordVersionModel.create(userId, passHashed, versionCounting);
-
-    //  3.b modify list password master to mark version
-    //     3.b.i if idx existed in list password -> version = config version - (idx + 1)
-    //     3.b.ii if idx non existed in list password -> version = -1
-    const modifyVersionPasswordMaster = [];
-    const passwordsHash = listPasswordsByConfigVersion.map(pass => pass.password_hash);
-    for (let idx = 0; idx < passwordInfo.length; idx++) {
-      if (passwordsHash.includes(passwordInfo[idx].password_hash)) {
-        modifyVersionPasswordMaster.push({
-          ...passwordInfo[idx],
-          version: versionCounting - 1 - idx
-        })
-      } else {
-        modifyVersionPasswordMaster.push({
-          ...passwordInfo[idx],
-          version: -1
-        })
-      }
-    }
-    for (const pass of modifyVersionPasswordMaster) {
-      await passwordVersionModel.updateVersion(userId, pass.id, pass.version);
-    }
-
-    //4. delete password has negative
-    await passwordVersionModel.deleteVersionNegativeByUserID(userId);
-
-    return false;
+    //3. if password input non exists - update password version and add new one
+    await updatePasswordVersionAccordingly(
+      userId,
+      passHashed,
+      versionCounting,
+      listPasswordsByConfigVersion,
+      passwordInfo
+    );
+    return passwordVersionExisted;
   }
 }
 
