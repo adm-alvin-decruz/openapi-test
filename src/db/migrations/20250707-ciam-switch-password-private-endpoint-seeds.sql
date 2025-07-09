2d85163c5a46b520f6f4c98447e4535923186b6f
Kwan Oun Liong <kwanoun.liong@1ITDMACDQ2K1H2YV1.local>
2025-07-09 13:06:56 +0800

    [CIAM-420] feat: update switch to 0

--- src/db/migrations/20250707-ciam-switch-password-private-endpoint-seeds.sql
+++ src/db/migrations/20250707-ciam-switch-password-private-endpoint-seeds.sql
@@ -1,8 +1,8 @@
 -- switch seed
 INSERT INTO `switches` (`name`, `switch`, `description`, `created_at`, `updated_at`)
 VALUES
-	('enable_password_versioning_private_endpoint', '1', 'Password versioning feature switch Private Endpoint', now(), now());
+	('enable_password_versioning_private_endpoint', '0', 'Password versioning feature switch Private Endpoint', now(), now());
 
 INSERT INTO `switches` (`name`, `switch`, `description`, `created_at`, `updated_at`)
 VALUES
-	('enable_check_password_complexity_private_endpoint', '1', 'Enable check password complexity base on IT policy suggestion Private Endpoint', now(), now());
+	('enable_check_password_complexity_private_endpoint', '0', 'Enable check password complexity base on IT policy suggestion Private Endpoint', now(), now());
