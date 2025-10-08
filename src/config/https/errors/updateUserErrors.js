db924969233cd43052096db9bc8e108f6681ef2c
vdr-synodus-quang <quangnhd+1@synodus.com>
2025-10-08 22:26:01 +0700

    CIAM-464 feature: incorrect status when retrieving membership with unregistered email

--- src/config/https/errors/updateUserErrors.js
+++ src/config/https/errors/updateUserErrors.js
@@ -4,36 +4,36 @@ class UpdateUserErrors {
   static ciamUpdateUserErr(lang) {
     return {
       membership: {
-        code: 200,
+        code: 400,
         mwgCode: 'MWG_CIAM_USER_UPDATE_ERR',
         message: messageLang('update_user_error', lang),
       },
       status: 'success',
-      statusCode: 200,
+      statusCode: 400,
     };
   }
   static ciamEmailNotExists(email, lang) {
     return {
       membership: {
-        code: 200,
+        code: 400,
         mwgCode: 'MWG_CIAM_USER_UPDATE_ERR',
         message: messageLang('email_not_being_used', lang),
         email: email,
       },
       status: 'success',
-      statusCode: 200,
+      statusCode: 400,
     };
   }
   static ciamNewEmailBeingUsedErr(email, lang) {
     return {
       membership: {
-        code: 200,
+        code: 400,
         mwgCode: 'MWG_CIAM_USER_UPDATE_ERR',
         message: messageLang('newEmail_being_used', lang),
         email: email,
       },
       status: 'success',
-      statusCode: 200,
+      statusCode: 400,
     };
   }
 }
