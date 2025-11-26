1d59be6c709d4650bb715ec9347d6462e71fbf81
Darren Leong <darren.leong@mandai.com>
2025-11-26 15:40:11 +0800

    #INC-15449 fix: Update App ID binding for APEX

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -6,13 +6,13 @@
 const appConfig = {
   // App ID DEV
   APP_ID_DEV:
-    '["aemDev.com.mandaiapi.ciam","gltDev.com.mandaiapi.ciam","tktDev.com.mandaiapi.ciam","passkit.Dev.internal.mandaiapi.ciam","rePassMicroSite.Dev.internal.mandaiapi.ciam","nopComm.Dev.service.mandaiapi.ciam", "loginMicroSite.Dev.internal.mandaiapi.ciam","mfaMobile.Dev.service.mandaiapi.ciam", "cliteOnline.Dev.internal.mandai.com", "entsvcDev.com.mandaiapi.ciam"]',
+    '["aemDev.com.mandaiapi.ciam","gltDev.com.mandaiapi.ciam","tktDev.com.mandaiapi.ciam","passkit.Dev.internal.mandaiapi.ciam","rePassMicroSite.Dev.internal.mandaiapi.ciam","nopComm.Dev.service.mandaiapi.ciam", "loginMicroSite.Dev.internal.mandaiapi.ciam","mfaMobile.Dev.service.mandaiapi.ciam", "cliteOnline.Dev.internal.mandai.com", "entsvcDev.com.mandaiapi.ciam", "apexDev.service.mandaiapi.ciam"]',
   // App ID UAT
   APP_ID_UAT:
-    '["aemUat.com.mandaiapi.ciam","gltUat.com.mandaiapi.ciam","tktUat.com.mandaiapi.ciam","passkit.Uat.internal.mandaiapi.ciam","rePassMicroSite.Uat.internal.mandaiapi.ciam","nopComm.Uat.service.mandaiapi.ciam", "loginMicroSite.Uat.internal.mandaiapi.ciam","mfaMobile.Uat.service.mandaiapi.ciam", "cliteOnline.Uat.internal.mandai.com", "entsvcUat.com.mandaiapi.ciam"]',
+    '["aemUat.com.mandaiapi.ciam","gltUat.com.mandaiapi.ciam","tktUat.com.mandaiapi.ciam","passkit.Uat.internal.mandaiapi.ciam","rePassMicroSite.Uat.internal.mandaiapi.ciam","nopComm.Uat.service.mandaiapi.ciam", "loginMicroSite.Uat.internal.mandaiapi.ciam","mfaMobile.Uat.service.mandaiapi.ciam", "cliteOnline.Uat.internal.mandai.com", "entsvcUat.com.mandaiapi.ciam", "apexUat.service.mandaiapi.ciam"]',
   // App ID Prod
   APP_ID_PROD:
-    '["aemProd.com.mandaiapi.ciam","gltProd.com.mandaiapi.ciam","tktProd.com.mandaiapi.ciam","passkit.Prod.internal.mandaiapi.ciam","rePassMicroSite.Prod.internal.mandaiapi.ciam","nopComm.Prod.service.mandaiapi.ciam", "loginMicroSite.Prod.internal.mandaiapi.ciam","mfaMobile.Prod.service.mandaiapi.ciam", "cliteOnline.Prod.internal.mandai.com", "entsvcProd.com.mandaiapi.ciam"]',
+    '["aemProd.com.mandaiapi.ciam","gltProd.com.mandaiapi.ciam","tktProd.com.mandaiapi.ciam","passkit.Prod.internal.mandaiapi.ciam","rePassMicroSite.Prod.internal.mandaiapi.ciam","nopComm.Prod.service.mandaiapi.ciam", "loginMicroSite.Prod.internal.mandaiapi.ciam","mfaMobile.Prod.service.mandaiapi.ciam", "cliteOnline.Prod.internal.mandai.com", "entsvcProd.com.mandaiapi.ciam", "apexProd.service.mandaiapi.ciam"]',
 
   // App ID Support DEV
   APP_ID_SUPPORT_DEV: '["support.Dev.internal.mandaiapi.ciam"]',
