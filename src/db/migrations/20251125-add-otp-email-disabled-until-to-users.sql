331bc206be765f488392812c55a5351d6d36deaa
hienvuarent <huyhienvu@gmail.com>
2025-12-02 10:44:31 +0700

    CIAM-557 fix: [PROD] Temporarily disabling OTP email sending for troubleshooting purpose

--- src/db/migrations/20251125-add-otp-email-disabled-until-to-users.sql
+++
@@ -1,4 +0,0 @@
--- Add otp_email_disabled_until column to users table
--- This field is used to temporarily disable OTP email sending for troubleshooting purposes
-ALTER TABLE users 
-ADD COLUMN IF NOT EXISTS otp_email_disabled_until DATETIME NULL DEFAULT NULL COMMENT 'Timestamp until which OTP email sending is disabled for troubleshooting';
